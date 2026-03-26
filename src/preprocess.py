36835b8ec61f44c3db04c866b2bfdc758bfea895
Abhay Verma <99489290+AbhayVerma9131@users.noreply.github.com>
2026-03-26 17:01:53 +0530

    Create preprocess.py

---
+++ src/preprocess.py
@@ -0,0 +1,12 @@
+import nltk
+from nltk.corpus import stopwords
+import string
+
+nltk.download('stopwords')
+
+def clean_text(text):
+    text = text.lower()
+    text = "".join([c for c in text if c not in string.punctuation])
+    words = text.split()
+    words = [w for w in words if w not in stopwords.words('english')]
+    return " ".join(words)
